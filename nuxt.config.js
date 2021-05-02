2fbc7f3bdc0485b3e59e106c8bbd345c2dfa64e6
ParkYoungWoong <thesecon@gmail.com>
2021-05-03 03:41:53 +0900

    Create Project

---
+++ nuxt.config.js
@@ -0,0 +1,63 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Nuxt Movie App',
+    htmlAttrs: {
+      lang: 'ko'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:site_name', property: 'og:type', content: 'Nuxt Movie App' },
+      { hid: 'og:title', property: 'og:type', content: 'Nuxt Movie App / Search' },
+      { hid: 'og:description', property: 'og:type', content: 'Hello Nuxt!' },
+      { hid: 'og:image', property: 'og:type', content: 'https://heropy.blog/css/images/logo.png' },
+      { hid: 'og:url', property: 'og:type', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Oswald:wght@500&family=Roboto:wght@400;700&display=swap' },
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/assets/css/main.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '~/plugins'
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/style-resources',
+    '@nuxtjs/dotenv'
+  ],
+
+  // @nuxtjs/style-resources
+  styleResources: {
+    scss: [
+      '~/scss/main.scss'
+    ]
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  serverMiddleware: [
+    { path: '/api/movie', handler: '~/server-middleware/movie.js' }
+  ]
+}
---
+++ store/about.js
@@ -0,0 +1,7 @@
+export const state = () => ({
+  name: 'HEROPY',
+  email: 'thesecon@gmail.com',
+  blog: 'https://heropy.blog',
+  phone: '+82-10-1234-5678',
+  image: 'https://heropy.blog/css/images/logo.png'
+})
